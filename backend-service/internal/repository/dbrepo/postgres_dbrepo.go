9f0e7fd78d48550cdcb6b232bf2a61399b1fa0f1
user.email <klikejo@gmail.com>
2024-12-06 20:38:52 +0300

    add transaction to db

--- backend-service/internal/repository/dbrepo/postgres_dbrepo.go
+++ backend-service/internal/repository/dbrepo/postgres_dbrepo.go
@@ -24,13 +24,18 @@ func (m *PostgresDBRepo) CreateUser(user models.User) (int, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), dbTimeOut)
 	defer cancel()
 
+	tx, err := m.DB.Begin()
+	if err != nil {
+		return 0, err
+	}
+
 	stmt := `INSERT INTO users (name, email, password, activated, created_at,
 		updated_at)
 		values ($1, $2, $3, $4, $5, $6) returning id`
 
 	var newID int
 
-	err := m.DB.QueryRowContext(ctx, stmt,
+	err = tx.QueryRowContext(ctx, stmt,
 		&user.Username,
 		&user.Email,
 		&user.PasswordHash,
@@ -40,8 +45,11 @@ func (m *PostgresDBRepo) CreateUser(user models.User) (int, error) {
 	).Scan(&newID)
 
 	if err != nil {
+		tx.Rollback()
 		return 0, err
 	}
+
+	tx.Commit()
 
 	return newID, nil
 }
@@ -49,6 +57,11 @@ func (m *PostgresDBRepo) CreateUser(user models.User) (int, error) {
 func (m *PostgresDBRepo) GetUserByUsername(username string) (*models.User, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), dbTimeOut)
 	defer cancel()
+
+	tx, err := m.DB.Begin()
+	if err != nil {
+		return nil, err
+	}
 
 	query := `SELECT 
 				id, name, email, password, activated, created_at, updated_at, version 
@@ -59,7 +72,7 @@ func (m *PostgresDBRepo) GetUserByUsername(username string) (*models.User, error
 
 	var u models.User
 
-	err := m.DB.QueryRowContext(ctx, query, username).Scan(
+	err = tx.QueryRowContext(ctx, query, username).Scan(
 		&u.ID,
 		&u.Username,
 		&u.Email,
@@ -71,6 +84,7 @@ func (m *PostgresDBRepo) GetUserByUsername(username string) (*models.User, error
 	)
 
 	if err != nil {
+		tx.Rollback()
 		switch {
 		case errors.Is(err, sql.ErrNoRows):
 			return nil, models.ErrRecordNotFound
@@ -79,12 +93,19 @@ func (m *PostgresDBRepo) GetUserByUsername(username string) (*models.User, error
 		}
 	}
 
-	return &u, err
+	tx.Commit()
+
+	return &u, nil
 }
 
 func (m *PostgresDBRepo) GetUserByID(id int64) (*models.User, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), dbTimeOut)
 	defer cancel()
+
+	tx, err := m.DB.Begin()
+	if err != nil {
+		return nil, err
+	}
 
 	query := `SELECT 
 				id, name, email, password, activated, created_at, updated_at, version
@@ -94,7 +115,7 @@ func (m *PostgresDBRepo) GetUserByID(id int64) (*models.User, error) {
 			   id = $1`
 
 	var u models.User
-	err := m.DB.QueryRowContext(ctx, query, id).Scan(
+	err = tx.QueryRowContext(ctx, query, id).Scan(
 		&u.ID,
 		&u.Username,
 		&u.Email,
@@ -105,10 +126,13 @@ func (m *PostgresDBRepo) GetUserByID(id int64) (*models.User, error) {
 		&u.Version,
 	)
 	if err != nil {
+		tx.Rollback()
 		return nil, err
 	}
 
-	return &u, err
+	tx.Commit()
+
+	return &u, nil
 }
 
 func (m *PostgresDBRepo) NewToken(userID int64, ttl time.Duration, scope string) (*models.Token, error) {
@@ -126,6 +150,11 @@ func (m *PostgresDBRepo) InsertToken(token *models.Token) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
+	tx, err := m.DB.Begin()
+	if err != nil {
+		return err
+	}
+
 	tokenHash := sha256.Sum256(token.Hash)
 
 	query := `INSERT INTO tokens (hash, user_id, expiry, scope)
@@ -133,30 +162,48 @@ func (m *PostgresDBRepo) InsertToken(token *models.Token) error {
 
 	args := []interface{}{tokenHash[:], token.UserID, token.Expiry, token.Scope}
 
-	_, err := m.DB.ExecContext(ctx, query, args...)
+	_, err = tx.ExecContext(ctx, query, args...)
 	if err != nil {
+		tx.Rollback()
 		return err
 	}
 
-	return err
+	tx.Commit()
+	return nil
 }
 
 func (m *PostgresDBRepo) DeleteAllTokensForUser(scope string, userID int64) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
+	tx, err := m.DB.Begin()
+	if err != nil {
+		return err
+	}
+
 	query := `DELETE FROM tokens
 			  WHERE scope = $1 AND user_id = $2`
 
-	_, err := m.DB.ExecContext(ctx, query, scope, userID)
+	_, err = tx.ExecContext(ctx, query, scope, userID)
 
-	return err
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	tx.Commit()
+	return nil
 }
 
 func (m *PostgresDBRepo) GetUserByToken(tokenScope, tokenPlainText string) (*models.User, error) {
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
+
+	tx, err := m.DB.Begin()
+	if err != nil {
+		return nil, err
+	}
 
 	query := `
 			SELECT users.id, users.created_at, users.name, users.email, users.password, users.activated, users.version
@@ -173,7 +220,7 @@ func (m *PostgresDBRepo) GetUserByToken(tokenScope, tokenPlainText string) (*mod
 
 	var user models.User
 
-	err := m.DB.QueryRowContext(ctx, query, args...).Scan(
+	err = tx.QueryRowContext(ctx, query, args...).Scan(
 		&user.ID,
 		&user.CreatedAt,
 		&user.Username,
@@ -183,6 +230,8 @@ func (m *PostgresDBRepo) GetUserByToken(tokenScope, tokenPlainText string) (*mod
 		&user.Version,
 	)
 	if err != nil {
+		tx.Rollback()
+
 		switch {
 		case errors.Is(err, sql.ErrNoRows):
 			return nil, models.ErrRecordNotFound
@@ -191,6 +240,8 @@ func (m *PostgresDBRepo) GetUserByToken(tokenScope, tokenPlainText string) (*mod
 		}
 	}
 
+	tx.Commit()
+	
 	return &user, nil
 }
 
@@ -199,15 +250,22 @@ func (m *PostgresDBRepo) NewArticle(article *models.Article) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	query := `INSERT INTO articles (name, publish, reading_time, username, html_list)
-	VALUES ($1, $2, $3, $4, $5);`
-
-	_, err := m.DB.ExecContext(ctx, query, article.Name, article.Publish, article.ReadingTime, article.Username, article.HtmlList)
-
+	tx, err := m.DB.Begin()
 	if err != nil {
 		return err
 	}
 
+	query := `INSERT INTO articles (name, publish, reading_time, username, html_list)
+	VALUES ($1, $2, $3, $4, $5);`
+
+	_, err = tx.ExecContext(ctx, query, article.Name, article.Publish, article.ReadingTime, article.Username, article.HtmlList)
+
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	tx.Commit()
 	return nil
 }
 
@@ -215,13 +273,18 @@ func (m *PostgresDBRepo) GetArticle(id int) (*models.Article, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
+	tx, err := m.DB.Begin()
+	if err != nil {
+		return nil, err
+	}
+
 	query := `SELECT name, publish, reading_time, username, html_list, created_at, updated_at, version 
 				FROM articles
 				WHERE id = $1`
 
 	var article models.Article
 
-	err := m.DB.QueryRowContext(ctx, query, id).Scan(
+	err = tx.QueryRowContext(ctx, query, id).Scan(
 		&article.Name,
 		&article.Publish,
 		&article.ReadingTime,
@@ -232,15 +295,22 @@ func (m *PostgresDBRepo) GetArticle(id int) (*models.Article, error) {
 		&article.Version,
 	)
 	if err != nil {
+		tx.Rollback()
 		return nil, err
 	}
 
+	tx.Commit()
 	return &article, nil
 }
 
 func (m *PostgresDBRepo) GetNews(page int) (*[]models.News, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
+
+	tx, err := m.DB.Begin()
+	if err != nil {
+		return nil, err
+	}
 
 	query := `SELECT id, name, created_at FROM articles WHERE publish=true ORDER BY created_at, id LIMIT $1 OFFSET $2`
 
@@ -249,8 +319,9 @@ func (m *PostgresDBRepo) GetNews(page int) (*[]models.News, error) {
 	limit := 9
 	offset := (page - 1) * 9
 
-	rows, err := m.DB.QueryContext(ctx, query, limit, offset)
+	rows, err := tx.QueryContext(ctx, query, limit, offset)
 	if err != nil {
+		tx.Rollback()
 		return nil, err
 	}
 
@@ -266,6 +337,7 @@ func (m *PostgresDBRepo) GetNews(page int) (*[]models.News, error) {
 		)
 
 		if err != nil {
+			tx.Rollback()
 			return nil, err
 		}
 
@@ -273,15 +345,23 @@ func (m *PostgresDBRepo) GetNews(page int) (*[]models.News, error) {
 	}
 
 	if err = rows.Err(); err != nil {
+		tx.Rollback()
 		return nil, err
 	}
 
+	tx.Commit()
 	return &newsArr, err
 }
 
 func (m *PostgresDBRepo) GetUserPermissions(token string) (*[]string, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
+
+	tx, err := m.DB.Begin()
+	if err != nil {
+		return nil, err
+	}
+
 	query := `  
 	SELECT 
 		code 
@@ -303,8 +383,9 @@ func (m *PostgresDBRepo) GetUserPermissions(token string) (*[]string, error) {
 		) as tokens ON users_permissions.user_id = tokens.user_id
 	) as tmp ON permissions.id = tmp.id`
 
-	rows, err := m.DB.QueryContext(ctx, query, token, models.ScopeActivation)
+	rows, err := tx.QueryContext(ctx, query, token, models.ScopeActivation)
 	if err != nil {
+		tx.Rollback()
 		return nil, err
 	}
 
@@ -317,6 +398,7 @@ func (m *PostgresDBRepo) GetUserPermissions(token string) (*[]string, error) {
 		)
 
 		if err != nil {
+			tx.Rollback()
 			return nil, err
 		}
 
@@ -324,8 +406,11 @@ func (m *PostgresDBRepo) GetUserPermissions(token string) (*[]string, error) {
 	}
 
 	if err = rows.Err(); err != nil {
+		tx.Rollback()
 		return nil, err
 	}
+
+	tx.Commit()
 
 	return &permissions, nil
 }
